99ecd5c026334df07ed8cb3d3ca9fa038b8db320
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-05-11 20:39:47 -0400

    changes made to manifest

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
     .then((reg) => {
       console.log('Service worker registered.', reg);
-    })
+    });
   })
 }
 
